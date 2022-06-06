2e418bed8287b23300ba9358c1d7d047f89c095a
Frowingg <edbico0@gmail.com>
2022-06-06 16:13:38 +0200

    form - discount

--- js/script.js
+++ js/script.js
@@ -8,5 +8,14 @@ generateButton.addEventListener('click',
         console.log(userKm)
         const userAge = document.getElementById('user-age').value;
         console.log(userAge)
+
+        price = 0.21 * userKm
+        if (userAge === 'minorenne') {
+            price -= (price/100*20);
+        }
+        else if (userAge === 'over 65') {
+            price -= (price/100*40);
+        }
+        console.log(price);
     }
 )
